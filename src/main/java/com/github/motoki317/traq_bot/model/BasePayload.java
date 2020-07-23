01b1fe691486c8f76bd02f3381ed9849c1534fe1
motoki317 <motoki317@gmail.com>
2020-07-23 20:08:07 +0900

    make event time public

--- src/main/java/com/github/motoki317/traq_bot/model/BasePayload.java
+++ src/main/java/com/github/motoki317/traq_bot/model/BasePayload.java
@@ -3,10 +3,14 @@ package com.github.motoki317.traq_bot.model;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-class BasePayload {
+public class BasePayload {
     final LocalDateTime eventTime;
 
     BasePayload(String eventTime) {
         this.eventTime = Timestamp.valueOf(eventTime.replace("T", " ").replace("Z", "")).toLocalDateTime();
+    }
+
+    public LocalDateTime getEventTime() {
+        return eventTime;
     }
 }
